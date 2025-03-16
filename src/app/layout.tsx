2b9ee18f89ed77b9fd37d10e92984cbcf5cebc29
Yelminoo <yelminoo2015@gmail.com>
2025-03-16 22:02:57 +0700

    add clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,6 +55,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+                (function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "qp62o29iir");
+              `,
+          }}
+        />
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased max-w-100 mx-auto pb-12 sm:pb-24 px-0",
