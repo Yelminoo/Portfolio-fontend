5d4a35695234f3d9c40535ec673cbd178cef1af7
Yelminoo <yelminoo2015@gmail.com>
2025-05-07 00:13:42 +0700

    update intro text section

--- src/components/bg.tsx
+++ src/components/bg.tsx
@@ -13,34 +13,30 @@ export function RetroGridDemo() {
         <AvatarImage alt={DATA.name} src={DATA.avatarUrl} />
         <AvatarFallback>{DATA.initials}</AvatarFallback>
       </Avatar>
-      <span className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] bg-clip-text text-left text-7xl font-bold leading-none tracking-tighter text-transparent">
+      <span className="pointer-events-none z-10 whitespace-normal bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] bg-clip-text text-left text-7xl font-bold leading-none tracking-tighter text-transparent">
         Hello I&apos;m <strong className="">Yel Min Oo </strong>
       </span>
 
       <GradualSpacing
-        className="text-wrap text-left text-md font-light -tracking-wide md:text-2xl "
+        className="text-left text-sm font-light tracking-normal md:text-2xl break-words"
         text="With expertise in both frontend and backend development,"
       />
       <RetroGrid />
       <GradualSpacing
-        className="text-wrap text-left text-md font-light -tracking-wide md:text-2xl"
-        text="I bring dynamic and robust web solutions to life. Whether "
+        className="text-left text-sm font-light tracking-normal md:text-2xl break-words"
+        text="I bring dynamic and robust web solutions to life. Whether"
       />
       <GradualSpacing
-        className="text-wrap text-left text-md font-light -tracking-wide md:text-2xl"
-        text="it's crafting seamless user experiences or building powerful server-side
-       "
-      />
-
-      <GradualSpacing
-        className="text-wrap text-left text-md font-light -tracking-wide md:text-2xl"
-        text="applications, I leverage my skills in JavaScript and PHP to deliver
-       "
+        className="text-left text-sm font-light tracking-normal md:text-2xl break-words"
+        text="it's crafting seamless user experiences or building powerful server-side"
       />
       <GradualSpacing
-        className="text-wrap text-left text-md font-light -tracking-wide md:text-2xl"
-        text="exceptional results. Let's create something amazing together.
-       "
+        className="text-left text-sm font-light tracking-normal md:text-2xl break-words"
+        text="applications, I leverage my skills in JavaScript and PHP to deliver"
+      />
+      <GradualSpacing
+        className="text-left text-sm font-light tracking-normal md:text-2xl break-words"
+        text="exceptional results. Let's create something amazing together."
       />
     </div>
   );
